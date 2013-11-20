f1dcb44ba2941f8bdc2644554234b5b387282259
Paul <jthotbox@gmail.com>
2013-11-20 10:36:57 +0200

    add sources

--- README.md
+++ README.md
@@ -2,3 +2,39 @@ angularJs.searchFilter
 ======================
 
 Easy (add new item) filter
+
+![alt tag](https://raw.github.com/JTOne123/angularJs.searchFilter/master/searchFilter.png)
+
+How to use:
+
+Html:
+<searchfilter items="filterItems" init-filter="initFilter" search-action="doSearch" />    
+
+Controller:
+$scope.filterItems =
+[
+	{ name: '_id', display: 'FileID', type: 'string' },
+	{ name: 'status', display: 'Status', type: 'dropdown', values: { 'New': 'new', 'Completed': 'completed', 'Deleted': 'deleted' } },
+	{ name: '_created.$dt', display: 'Created', type: 'date' },
+	{
+		name: '_$orderBy', display: '[Order By]', type: 'dropdown', values: {
+			'Created descent': {
+				field: '_created.$dt',
+				direction: 'desc'
+			},
+			'Created ascent': {
+				field: '_created.$dt',
+				direction: 'asc'
+			}
+		}
+	},
+	{ name: 'originalFileName', display: 'Original filename', type: 'string' },
+	{ name: 'extId', display: 'ExternalID' },
+	{ name: 'size', display: 'Size' },
+	{ name: '_modified.$dt', display: 'Modified', type: 'date' },
+	{ name: 'lastAccess.$dt', display: 'Last access', type: 'date' },
+	{ name: 'mappings.storage', display: 'Storage', type: 'dropdown', values: StorageService.getStorageMap() }
+];
+
+
+CSS: By defaults used a twitter bootstrap
---
+++ searchfilter.js
@@ -0,0 +1,125 @@
+﻿angular
+    .module('gdnui')
+    .directive('searchfilter', function ($compile, $timeout, $q, config, AuthService) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                items: '=',
+                initFilter: '=',
+                filterPath: '@',
+                searchAction: '&'
+            },
+            transclude: true,
+            templateUrl: 'templates/components/searchFilter.html',
+            link: function (scope, element, attrs) {
+                scope.filter = {};
+
+                scope.addFilter = function () {
+                    scope.filter[scope.selectedItem.name] = scope.selectedItem.searchVal;
+                    scope.wrapFilter[scope.selectedItem.name] = scope.selectedItem;
+                    scope.selectedItem = null;
+                };
+
+                scope.select = function (selectedItem) {
+                    scope.selectedItem = selectedItem;
+
+                    $q.when(selectedItem.values).then(function (values) {
+                        var key = _.keys(values)[0];
+                        scope.selectedItem.searchVal = values[key];
+                        scope.selectedItem.displayVal = key;
+                    });
+                };
+
+                scope.removeFilterItem = function (f) {
+                    delete scope.filter[f.name];
+                    delete scope.wrapFilter[f.name];
+                };
+
+                scope.isEmpty = function (obj) {
+                    return _.isEmpty(obj);
+                };
+
+                scope.isLast = function (obj, list) {
+                    var l = _.last(_.toArray(list));
+                    return _.isEqual(obj, l);
+                };
+
+                scope.displayValue = function (val) {
+                    if (_.isObject(val)) {
+                        var result = '';
+
+                        if (val._from)
+                            result += 'from: ' + val._from + ' ';
+
+                        if (val._to)
+                            result += 'to: ' + val._to;
+
+                        if (val.field && val.direction)
+                            result += val.field + ' ' + val.direction.toUpperCase();
+
+                        return result;
+                    } else {
+                        return val;
+                    }
+                };
+
+                scope.doSearch = function () {
+                    if (scope.selectedItem && scope.selectedItem.name && scope.selectedItem.searchVal)
+                        scope.addFilter();
+
+                    scope.searchAction && (scope.searchAction())(scope.filter);
+                };
+
+                scope.init = function () {
+                    scope.availableItems = scope.items;
+                    scope.wrapFilter = {};
+                    scope.authService = AuthService;
+
+                    if (scope.availableItems.length > 0)
+                        scope.select(scope.availableItems[0]);
+
+                    angular
+                        .element('.datePicker')
+                        .datepicker({ format: config.dateFormat })
+                        .on('changeDate', function (ev) {
+
+                            var model = angular.element(ev.target).attr('ng-model');
+                            var splitedModel = model.split('.');
+
+                            var selectedDate = ev.date;
+                            if (splitedModel[splitedModel.length - 1] == '_to')
+                                selectedDate = new Date(selectedDate.setSeconds(24 * 60 * 60 - 1));
+
+                            splitedModel[splitedModel.length - 1] = '_$specialValue' + splitedModel[splitedModel.length - 1];
+                            var specialModel = splitedModel.join('.');
+
+                            scope.$eval(
+                                model + '="' + angular.element(ev.target).val() + '";' +
+                                specialModel + '="' + selectedDate.toISOString() + '";'
+                            );
+
+                            scope.$digest();
+                        });
+
+                    if (scope.initFilter) {
+                        _.each(scope.initFilter, function (val, key) {
+
+                            scope.selectedItem = _.find(scope.items, function (f) {
+                                return f.name == key;
+                            });
+
+                            if (scope.selectedItem) {
+                                scope.selectedItem.searchVal = val;
+                                scope.addFilter();
+                            }
+                        });
+                    }
+                };
+
+                $timeout(function () {
+                    scope.init();
+                });
+            }
+        };
+    });
